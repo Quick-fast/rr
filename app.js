8af12bf998d3927bdb6e28ae399c96e53016e08d
Richieharrison <ngatiarich@gmail.com>
2023-04-14 17:54:25 +0300

    Finnesssing the prices

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post('/order', async(req, res)=>{
         destination: req.body.destination,
         phoneNumber: req.body.phoneNumber,
         distance: Math.ceil(req.body.distance),
-        amountPaid: Math.ceil(req.body.googleDistance),
+        amountPaid: Math.ceil(req.body.amountPaid),
     })
 
     await order.save()
